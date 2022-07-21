b81f44a780ce824d5cbb7ecded670085e624522e
Desmond Williams <demowi8@gmail.com>
2022-07-21 16:11:35 -0400

    nav bar component and styles

--- pitterpatter-app/src/App.js
+++ pitterpatter-app/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import './components/navbar'
+import NavBar from './components/navbar';
 
 function App() {
-  return (
+  return (<div className='App'>
     <NavBar />
+  </div>
+    
   );
 }
 
--- pitterpatter-app/src/components/navbar.js
+++ pitterpatter-app/src/components/navbar.js
@@ -1,21 +1,13 @@
 import './navbar.css'
 const NavBar = () => {
-    return  
-<>
+    return  <>
     <div className="navbar" id="main-navbar">
-        <div className="navbar-col1">
+        <div className="navbar-col">
             <h2 className="navbar-logo">Pitter Patter</h2>
-        </div>
-
-        <div className="navbar-col2">
-            <div>
-                <img className="brand" src="./assets/twitter-seeklogo.com.svg" class="brand"></img>
-            </div>
         </div>
     </div>
 </>
 }
-
 export default NavBar;
 
 
