782dbf8524f999de54052fa166bacee31b5b78b3
Desmond Williams <demowi8@gmail.com>
2022-07-22 11:56:23 -0400

    form logic need to solve useState error

--- pitterpatter-app/src/App.js
+++ pitterpatter-app/src/App.js
@@ -1,10 +1,51 @@
 import './App.css';
+import FormInput from './components/FormInput';
 import './components/navbar'
 import NavBar from './components/navbar';
 
 function App() {
+  const [profileValues, setProfileValues] = useState({
+    username:"",
+    password:"",
+  });
+
+  const inputs = [{
+    id: 1,
+    name:'username',
+    type:'text',
+    placeholder:'Username',
+    label:'Username'
+  },
+  {
+    id: 2,
+    name:'password',
+    type:'text',
+    placeholder:'Password',
+    label:'Password'
+  }]
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+  };
+
+  const onChange = (e)=>{
+    setProfileValues({...profileValues, [e.target.name]: e.target.value });
+  }
   return (<div className='App'>
     <NavBar />
+
+    <form className='loginCard' onSubmit={handleSubmit}>
+      {inputs.map((input) => (
+        <FormInput 
+        key={input.id} 
+        {...input} 
+        value={profileValues[input.name]}
+        onChange={onChange}/>
+
+      ))}
+
+    <button>Submit</button>
+    </form>
   </div>
     
   );
