8fe5646613b7e79c6930a614542865ad8056d14f
Desmond Williams <demowi8@gmail.com>
2022-07-24 14:16:22 -0400

    changed styles

--- pitterpatter-app/src/components/FormInput.js
+++ pitterpatter-app/src/components/FormInput.js
@@ -1,12 +1,12 @@
 import './FormInput.css';
 
 const FormInput = (props) => {
-    const {label, onChange, id, ...inputProps } = props;
+    const {label, errorMessage, onChange, id, ...inputProps } = props;
     return (
-        <div >
-            <label>{label}</label>
+        <div>
+            <label >{label}</label>
             <input {...inputProps} onChange={onChange} className='login'/>
-
+            <span>{errorMessage}</span>
         </div>
     );
 }
