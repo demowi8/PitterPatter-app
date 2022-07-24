bbd060210976d4b08052d771e1ffc748e81480b0
Desmond Williams <demowi8@gmail.com>
2022-07-24 16:18:41 -0400

    added auth context wrapper to routes

--- pitterpatter-app/src/App.js
+++ pitterpatter-app/src/App.js
@@ -4,24 +4,27 @@ import NavBar from './components/navbar';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import Account from './components/Account';
+import { AuthContextProvider } from './context/AuthContext';
 
 
 function App() {
-  //2 function button login and register
+  //3 function button login and register and logout
   // login button checks if username&password are in database and if they are they show profile card, create tweets, and tweets from user and other users
   // register button creates a new username&password and adds to database then logs profile in
-
+// logout returns to home page signin/signup
 
 
   return (<div className='App'>
     <NavBar />
 
 
-    <Routes>
-      <Route path='/' element={<SignIn />} />
-      <Route path='/signup' element={<SignUp />} />
-      <Route path='/account' element={<Account />} />
-    </Routes>
+    <AuthContextProvider>
+      <Routes>
+        <Route path='/' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/account' element={<Account />} />
+      </Routes>
+    </AuthContextProvider>
 
   </div>
     
