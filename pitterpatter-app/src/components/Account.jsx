d75c1278d14780f2ade07fe438bf48f9d6a14d47
Desmond Williams <demowi8@gmail.com>
2022-07-24 17:59:05 -0400

    logout button functionality  and navigation

--- pitterpatter-app/src/components/Account.jsx
+++ pitterpatter-app/src/components/Account.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
 import './Account.css';
 
 const Account = (props) => {
     const {user, logout} = UserAuth();
+    const navigate = useNavigate();
 
+    const handleLogout = async () => {
+        try {
+            await logout();
+            navigate('/')
+            console.log('Logged out');
+        } catch (e) {
+            console.log(e.message)
+        }
+    }
     return (
         <div>
-            <h1>Hi {user.email}</h1>
+            <h1>Welcome {user && user.email}!</h1>
 
-            <button className='LogBtn'>Logout</button>
+            <button onClick={handleLogout} className='LogBtn'>Logout</button>
         </div>
     )
 }
